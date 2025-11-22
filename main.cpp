62db68832219a7a03e2c88e0b5389b73b874c6a0
BosSCaoMa <1741925335@qq.com>
2025-11-22 23:59:01 +0800

    test_ok

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@
 int main() {
     LOG_DEBUG("This is a debug message: x=%d", 42);
     LOG_INFO("This is an info message");
+    LOG_ERROR("This is an error message, %s", "something went wrong");
     return 0;
 }
